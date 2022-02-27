e7340a406ce4ae1aa1e15c91990204ad0707d259
Fachrezandi20 <andi.muh.ft20@mail.umy.ac.id>
2022-02-27 19:57:02 +0700

    Initial commit

---
+++ app/src/main/java/com/example/a20200140050_act1_bbagian4/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a20200140050_act1_bbagian4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
